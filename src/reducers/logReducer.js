f3ed2af6a65bae3df3f25b03c989ad615efb527d
Tassos Vassilopoulos <nvrsr@hotmail.com>
2021-05-03 17:30:21 +0300

    Redux: 1) logActions for logs component 2) Actions --> types for reducer 3) Logs component "connect" and mapStateToProps 4) rootReducer with combineReducers 5) logReducer for Logs component

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,41 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SEARCH_LOGS,
+  SET_CURRENT,
+  CLEAR_CURRENT
+} from '../actions/types';
+
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+ switch(action.type) {
+        case GET_LOGS:
+            return {
+                ...state,
+                logs: action.payload,
+                loading: false
+            }
+        case SET_LOADING:
+            return {
+                ...state,
+                loading:true
+            }
+        case LOGS_ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state
+    }
+};
